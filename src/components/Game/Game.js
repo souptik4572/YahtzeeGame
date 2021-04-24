9e4da8295789b10fb4a2db00a25143e351d2eed2
souptik4572 <souptiksarkar4572@gmail.com>
2021-04-24 14:48:57 +0530

    implemented small straight score calculator

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,14 +5,23 @@ import { v4 as uuidv4 } from 'uuid';
 import Dice from '../Dice/Dice';
 import Score from '../Score/Score';
 import { pickRandomValue, numberToWords } from '../../helpers/helper';
-import { individualScore, checkThreeOfKind, checkFourOfKind, checkFullHouse, checkLargeStraight, checkYahtzee, sumAllValues } from '../../helpers/scoreHelper';
+import {
+	individualScore,
+	checkThreeOfKind,
+	checkFourOfKind,
+	checkFullHouse,
+	checkSmallStraight,
+	checkLargeStraight,
+	checkYahtzee,
+	sumAllValues,
+} from '../../helpers/scoreHelper';
 
-const Rolls_Left = 3;
-const Total_Rounds = 12;
+const ROLLS_LEFT = 3;
+const TOTAL_ROUNDS = 12;
 
 class Game extends Component {
 	static defaultProps = {
-		dice: 5
+		dice: 5,
 	};
 	constructor(props) {
 		super(props);
@@ -21,31 +30,32 @@ class Game extends Component {
 				return {
 					id: uuidv4(),
 					isLocked: false,
-					value: pickRandomValue()
+					value: pickRandomValue(),
 				};
 			}),
-			rollsLeft: Rolls_Left,
-            totalScore: 0,
-            totalRounds: Total_Rounds,
-            upperScore: Array.from({ length: 6 }).map(() => undefined),
-            threeOfKind: undefined,
-            fourOfKind: undefined,
-            fullHouse: undefined,
-            smallStraight: undefined,
-            largeStraight: undefined,
-            yahtzee: undefined,
-            chance: undefined
+			rollsLeft: ROLLS_LEFT,
+			totalScore: 0,
+			totalRounds: TOTAL_ROUNDS,
+			upperScore: Array.from({ length: 6 }).map(() => undefined),
+			threeOfKind: undefined,
+			fourOfKind: undefined,
+			fullHouse: undefined,
+			smallStraight: undefined,
+			largeStraight: undefined,
+			yahtzee: undefined,
+			chance: undefined,
 		};
 		this.handleRoll = this.handleRoll.bind(this);
-        this.handleLock = this.handleLock.bind(this);
-        this.assignUpperScore = this.assignUpperScore.bind(this);
-        this.assignThreeOfKind = this.assignThreeOfKind.bind(this);
-        this.assignFourOfKind = this.assignFourOfKind.bind(this);
-        this.assignChance = this.assignChance.bind(this);
-        this.assignFullHouse = this.assignFullHouse.bind(this);
-        this.assignYahtzee = this.assignYahtzee.bind(this);
-        this.assignLargeStraight = this.assignLargeStraight.bind(this);
-        this.handleRestart = this.handleRestart.bind(this);
+		this.handleLock = this.handleLock.bind(this);
+		this.assignUpperScore = this.assignUpperScore.bind(this);
+		this.assignThreeOfKind = this.assignThreeOfKind.bind(this);
+		this.assignFourOfKind = this.assignFourOfKind.bind(this);
+		this.assignChance = this.assignChance.bind(this);
+		this.assignFullHouse = this.assignFullHouse.bind(this);
+		this.assignYahtzee = this.assignYahtzee.bind(this);
+		this.assignSmallStraight = this.assignSmallStraight.bind(this);
+		this.assignLargeStraight = this.assignLargeStraight.bind(this);
+		this.handleRestart = this.handleRestart.bind(this);
 	}
 	handleRoll() {
 		const newDiceValues = this.state.dice.map((die) => {
@@ -62,101 +72,115 @@ class Game extends Component {
 			return die;
 		});
 		this.setState({ dice: newDiceLocks });
-    }
-    increaseScore_decreaseRound(newScore) {
-        this.setState({ totalScore: this.state.totalScore + newScore, totalRounds: this.state.totalRounds - 1 });
-        this.handleRoll();
-        this.setState({ rollsLeft: Rolls_Left });
-    }
-    assignUpperScore(id) {
-        if(this.state.upperScore[id] === undefined){
-            const newUpperScore = this.state.upperScore;
-            newUpperScore[id] = individualScore(id+1, this.state.dice);
-            this.setState({ upperScore: newUpperScore });
-            this.increaseScore_decreaseRound(newUpperScore[id]);
-        }
-    }
-    assignThreeOfKind(id) {
-        if(this.state.threeOfKind === undefined){
-            const threeScore = checkThreeOfKind(this.state.dice);
-            this.setState({ threeOfKind: threeScore });
-            this.increaseScore_decreaseRound(threeScore);
-        }
-    }
-    assignFourOfKind(id) {
-        if(this.state.fourOfKind === undefined){
-            const fourScore = checkFourOfKind(this.state.dice);
-            this.setState({ fourOfKind: fourScore });
-            this.increaseScore_decreaseRound(fourScore);
-        }
-    }
-    assignFullHouse(id) {
-        if(this.state.fullHouse === undefined){
-            const fullHouseScore = checkFullHouse(this.state.dice);
-            this.setState({ fullHouse: fullHouseScore });
-            this.increaseScore_decreaseRound(fullHouseScore);
-        }
-    }
-    assignLargeStraight(id) {
-        if(this.state.largeStraight === undefined) {
-            const largeStraightScore = checkLargeStraight(this.state.dice);
-            this.setState({ largeStraight: largeStraightScore });
-            this.increaseScore_decreaseRound(largeStraightScore);
-        }
-    }
-    assignYahtzee(id) {
-        if(this.state.yahtzee === undefined){
-            const yahtzeeScore = checkYahtzee(this.state.dice);
-            this.setState({ yahtzee: yahtzeeScore });
-            this.increaseScore_decreaseRound(yahtzeeScore);  
-        }
-    }
-    assignChance(id) {
-        const chanceScore = sumAllValues(this.state.dice);
-        if(this.state.chance === undefined){
-            this.setState({ chance: chanceScore });
-            this.increaseScore_decreaseRound(chanceScore);
-        }
-    }
-    handleRestart() {
-        this.setState({
-            dice: Array.from({ length: this.props.dice }).map(() => {
+	}
+	increaseScore_decreaseRound(newScore) {
+		this.setState({
+			totalScore: this.state.totalScore + newScore,
+			totalRounds: this.state.totalRounds - 1,
+		});
+		this.handleRoll();
+		this.setState({ rollsLeft: ROLLS_LEFT });
+	}
+	assignUpperScore(id) {
+		if (this.state.upperScore[id] === undefined) {
+			const newUpperScore = this.state.upperScore;
+			newUpperScore[id] = individualScore(id + 1, this.state.dice);
+			this.setState({ upperScore: newUpperScore });
+			this.increaseScore_decreaseRound(newUpperScore[id]);
+		}
+	}
+	assignThreeOfKind(id) {
+		if (this.state.threeOfKind === undefined) {
+			const threeScore = checkThreeOfKind(this.state.dice);
+			this.setState({ threeOfKind: threeScore });
+			this.increaseScore_decreaseRound(threeScore);
+		}
+	}
+	assignFourOfKind(id) {
+		if (this.state.fourOfKind === undefined) {
+			const fourScore = checkFourOfKind(this.state.dice);
+			this.setState({ fourOfKind: fourScore });
+			this.increaseScore_decreaseRound(fourScore);
+		}
+	}
+	assignFullHouse(id) {
+		if (this.state.fullHouse === undefined) {
+			const fullHouseScore = checkFullHouse(this.state.dice);
+			this.setState({ fullHouse: fullHouseScore });
+			this.increaseScore_decreaseRound(fullHouseScore);
+		}
+	}
+	assignSmallStraight(id) {
+		if (this.state.smallStraight === undefined) {
+			const smallStraightScore = checkSmallStraight(this.state.dice);
+			this.setState({ smallStraight: smallStraightScore });
+			this.increaseScore_decreaseRound(smallStraightScore);
+		}
+	}
+	assignLargeStraight(id) {
+		if (this.state.largeStraight === undefined) {
+			const largeStraightScore = checkLargeStraight(this.state.dice);
+			this.setState({ largeStraight: largeStraightScore });
+			this.increaseScore_decreaseRound(largeStraightScore);
+		}
+	}
+	assignYahtzee(id) {
+		if (this.state.yahtzee === undefined) {
+			const yahtzeeScore = checkYahtzee(this.state.dice);
+			this.setState({ yahtzee: yahtzeeScore });
+			this.increaseScore_decreaseRound(yahtzeeScore);
+		}
+	}
+	assignChance(id) {
+		const chanceScore = sumAllValues(this.state.dice);
+		if (this.state.chance === undefined) {
+			this.setState({ chance: chanceScore });
+			this.increaseScore_decreaseRound(chanceScore);
+		}
+	}
+	handleRestart() {
+		this.setState({
+			dice: Array.from({ length: this.props.dice }).map(() => {
 				return {
 					id: uuidv4(),
 					isLocked: false,
-					value: pickRandomValue()
+					value: pickRandomValue(),
 				};
 			}),
-			rollsLeft: Rolls_Left,
-            totalScore: 0,
-            totalRounds: Total_Rounds,
-            upperScore: Array.from({ length: 6 }).map(() => undefined),
-            threeOfKind: undefined,
-            fourOfKind: undefined,
-            fullHouse: undefined,
-            smallStraight: undefined,
-            largeStraight: undefined,
-            yahtzee: undefined,
-            chance: undefined
-        })
-    }
+			rollsLeft: ROLLS_LEFT,
+			totalScore: 0,
+			totalRounds: TOTAL_ROUNDS,
+			upperScore: Array.from({ length: 6 }).map(() => undefined),
+			threeOfKind: undefined,
+			fourOfKind: undefined,
+			fullHouse: undefined,
+			smallStraight: undefined,
+			largeStraight: undefined,
+			yahtzee: undefined,
+			chance: undefined,
+		});
+	}
 	render() {
 		const scoreUpperPart = this.state.upperScore.map((element, idx) => {
 			return (
 				<Score
-                    key={idx}
-                    id={idx}
+					key={idx}
+					id={idx}
 					scoreName={numberToWords(idx + 1)}
-                    scoreValue={element === undefined ? `${idx + 1} point per ${idx + 1}` : element}
-                    assignScore={this.assignUpperScore}
+					scoreValue={element === undefined ? `${idx + 1} point per ${idx + 1}` : element}
+					assignScore={this.assignUpperScore}
 				/>
 			);
 		});
 		return (
-			<div className="Game">
-				<div className="Game-dice">
+			<div className='Game'>
+				<div className='Game-dice'>
 					<p>Yahtzee!</p>
-					<Dice nDie={this.props.dice} diceValues={this.state.dice} lock={this.handleLock} />
+					<Dice
+						nDie={this.props.dice}
+						diceValues={this.state.dice}
+						lock={this.handleLock}
+					/>
 					<button
 						className={!!!this.state.rollsLeft ? 'Game-lockedButton' : ''}
 						onClick={this.handleRoll}
@@ -166,24 +190,89 @@ class Game extends Component {
 						{this.state.rollsLeft} Rolls Left{' '}
 					</button>
 				</div>
-				<div className="Game-score">
-                    <div className="Game-score-parts">
-                        <h1>Upper</h1>
-                        {scoreUpperPart}
-                        <h1>Lower</h1>
-                        <Score id={1} scoreName="three of kind" scoreValue={this.state.threeOfKind === undefined ? "Sum all dice if 3 are the same" : this.state.threeOfKind} assignScore={this.assignThreeOfKind} />
-                        <Score id={1} scoreName="four of kind" scoreValue={this.state.fourOfKind === undefined ? "Sum all dice if 4 are the same" : this.state.fourOfKind} assignScore={this.assignFourOfKind} />
-                        <Score id={1} scoreName="full house" scoreValue={this.state.fullHouse === undefined ? "25 points for a full house" : this.state.fullHouse} assignScore={this.assignFullHouse} />
-                        <Score id={1} scoreName="small straight" scoreValue={this.state.smallStraight === undefined ? "30 points for a small straight" : this.state.smallStraight} />
-                        <Score id={1} scoreName="large straight" scoreValue={this.state.largeStraight === undefined ? "40 points for a large straight" : this.state.largeStraight} assignScore={this.assignLargeStraight} />
-                        <Score id={1} scoreName="yahtzee" scoreValue={this.state.yahtzee === undefined ? "50 points for yahtzee" : this.state.yahtzee} assignScore={this.assignYahtzee} />
-                        <Score id={1} scoreName="chance" scoreValue={this.state.chance === undefined ? "Sum of all dice" : this.state.chance}  assignScore={this.assignChance} />
-                    </div>
-                    <div className="Game-total-score">
-                        <h1>total score: {this.state.totalScore} </h1>
-                    </div>
-                    <button className="Game-restart" onClick={this.handleRestart}>play again</button>
-                </div>
+				<div className='Game-score'>
+					<div className='Game-score-parts'>
+						<h1>Upper</h1>
+						{scoreUpperPart}
+						<h1>Lower</h1>
+						<Score
+							id={1}
+							scoreName='three of kind'
+							scoreValue={
+								this.state.threeOfKind === undefined
+									? 'Sum all dice if 3 are the same'
+									: this.state.threeOfKind
+							}
+							assignScore={this.assignThreeOfKind}
+						/>
+						<Score
+							id={1}
+							scoreName='four of kind'
+							scoreValue={
+								this.state.fourOfKind === undefined
+									? 'Sum all dice if 4 are the same'
+									: this.state.fourOfKind
+							}
+							assignScore={this.assignFourOfKind}
+						/>
+						<Score
+							id={1}
+							scoreName='full house'
+							scoreValue={
+								this.state.fullHouse === undefined
+									? '25 points for a full house'
+									: this.state.fullHouse
+							}
+							assignScore={this.assignFullHouse}
+						/>
+						<Score
+							id={1}
+							scoreName='small straight'
+							scoreValue={
+								this.state.smallStraight === undefined
+									? '30 points for a small straight'
+									: this.state.smallStraight
+							}
+							assignScore={this.assignSmallStraight}
+						/>
+						<Score
+							id={1}
+							scoreName='large straight'
+							scoreValue={
+								this.state.largeStraight === undefined
+									? '40 points for a large straight'
+									: this.state.largeStraight
+							}
+							assignScore={this.assignLargeStraight}
+						/>
+						<Score
+							id={1}
+							scoreName='yahtzee'
+							scoreValue={
+								this.state.yahtzee === undefined
+									? '50 points for yahtzee'
+									: this.state.yahtzee
+							}
+							assignScore={this.assignYahtzee}
+						/>
+						<Score
+							id={1}
+							scoreName='chance'
+							scoreValue={
+								this.state.chance === undefined
+									? 'Sum of all dice'
+									: this.state.chance
+							}
+							assignScore={this.assignChance}
+						/>
+					</div>
+					<div className='Game-total-score'>
+						<h1>total score: {this.state.totalScore} </h1>
+					</div>
+					<button className='Game-restart' onClick={this.handleRestart}>
+						play again
+					</button>
+				</div>
 			</div>
 		);
 	}
--- src/helpers/scoreHelper.js
+++ src/helpers/scoreHelper.js
@@ -18,6 +18,13 @@ function countFrequency(diceValues) {
 	return dict;
 }
 
+function getRawDiceValues(diceValues) {
+	const rawDiceValues = diceValues.map((aDiceValue) => {
+		return aDiceValue.value;
+	});
+	return rawDiceValues;
+}
+
 function checkThreeOfKind(diceValues) {
 	const dict = countFrequency(diceValues);
 	for (let keys in dict) {
@@ -45,33 +52,50 @@ function checkFullHouse(diceValues) {
 	else if (
 		(dict[dictKeys[0]] === 2 && dict[dictKeys[1]] === 3) ||
 		(dict[dictKeys[0]] === 3 && dict[dictKeys[1]] === 2)
-    )   return 25;    
+	)
+		return 25;
+	return 0;
+}
+
+function checkSmallStraight(diceValues) {
+	const diceSet = new Set(getRawDiceValues(diceValues));
+	console.log(diceSet);
+	if (diceSet.has(2) && diceSet.has(3) && diceSet.has(4) && (diceSet.has(5) || diceSet.has(1)))
+		return 30;
+	if (diceSet.has(3) && diceSet.has(4) && diceSet.has(5) && diceSet.has(6)) return 30;
 	return 0;
 }
 
 function checkLargeStraight(diceValues) {
 	const dict = countFrequency(diceValues);
-	if(Object.keys(dict).length === 5)
-		return 40;
+	if (Object.keys(dict).length === 5) return 40;
 	return 0;
 }
 
 function checkYahtzee(diceValues) {
-    let currentValue = diceValues[0].value;
-    for(let i=1; i<diceValues.length; i++){
-        if(currentValue !== diceValues[i].value)
-            return 0;
-        currentValue = diceValues[i].value;
-    }
-    return 50;
+	let currentValue = diceValues[0].value;
+	for (let i = 1; i < diceValues.length; i++) {
+		if (currentValue !== diceValues[i].value) return 0;
+		currentValue = diceValues[i].value;
+	}
+	return 50;
 }
 
 function sumAllValues(diceValues) {
 	let sum = 0;
 	diceValues.forEach((element) => {
-        sum += element.value;
+		sum += element.value;
 	});
 	return sum;
 }
 
-export { individualScore, checkThreeOfKind, checkFourOfKind, checkFullHouse, checkLargeStraight, checkYahtzee, sumAllValues };
+export {
+	individualScore,
+	checkThreeOfKind,
+	checkFourOfKind,
+	checkFullHouse,
+	checkSmallStraight,
+	checkLargeStraight,
+	checkYahtzee,
+	sumAllValues,
+};
